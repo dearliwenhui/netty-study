2b0c9b1761b8a3dbf2ca4696db2b877261fba8bd
dearliwenhui <dearliwenhui@gmail.com>
2023-02-20 23:26:28 +0800

    Update EchoServer.java

--- ketang-tl-netty/netty-basic/src/main/java/cn/tuling/nettybasic/echo/EchoServer.java
+++ ketang-tl-netty/netty-basic/src/main/java/cn/tuling/nettybasic/echo/EchoServer.java
@@ -45,13 +45,15 @@ public class EchoServer  {
             .channel(NioServerSocketChannel.class)/*指定使用NIO的通信模式*/
             .localAddress(new InetSocketAddress(port))/*指定监听端口*/
                     //.childOption()
-            //.handler();
+            //.handler(); /*handler() 作用于ServerSocketChannel ;childHandler()作用于接收客户端连接后，ServerSocketChannel生成的SocketChannel，可以参考第2节课笔记*/
             .childHandler(new ChannelInitializer<SocketChannel>() {
                 @Override
                 protected void initChannel(SocketChannel ch) throws Exception {
                     ch.pipeline().addLast(serverHandler);
                 }
             });
+            //将端口和当前应用进行绑定，在netty的操作中，基本上都是异步的，bind()方法会产生一个ChannelFuture，说明bind方法是异步操作。
+            //当代码执行到这里，不会阻塞等待绑定完成，所以加上sync()会阻塞到bind完成
             ChannelFuture f = b.bind().sync();/*异步绑定到服务器，sync()会阻塞到完成*/
             f.channel().closeFuture().sync();/*阻塞当前线程，直到服务器的ServerChannel被关闭*/
         } finally {
